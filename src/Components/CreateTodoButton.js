41b7a9f036e3e75b60f1e2bdfe7359f7f583f7f1
Ali-2121 <prds20bnll@gmail.com>
2024-11-01 14:15:29 -0600

    Clase 6: Aprendiendo sobre eventos 'onClick', 'onChange'

--- src/Components/CreateTodoButton.js
+++ src/Components/CreateTodoButton.js
@@ -1,8 +1,17 @@
 import './../css/CreateTodoButton.css';
 
-function CreateTodoButton()
-{
-  return(<button className="CreateTodoButton">+</button>);
+function CreateTodoButton(){
+  return(
+    <button
+     className="CreateTodoButton"
+     onClick={(event)=>{
+      console.log("clikiii")
+      console.log(event)
+      console.log(event.target)
+     }
+     }
+     >+</button>
+  );
 }
 
 export { CreateTodoButton};
--- src/Components/TodoSearch.js
+++ src/Components/TodoSearch.js
@@ -5,6 +5,11 @@ function TodoSearch() {
     <input
       placeholder="Ingresa una actividad"
       className="TodoSearch"
+      onChange={(event)=>{
+        console.log("escribiendo")
+        console.log(event.target.value)
+        console.log(event.target)
+      }}
     />
   );
 }
