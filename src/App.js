786e0c12985b1695e8a8b2e6080ec5c069c21804
Ali-2121 <prds20bnll@gmail.com>
2024-11-21 12:50:22 -0600

    Clase 8: Aprendiendo a usar estados derivados

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import { CreateTodoButton } from './Components/CreateTodoButton';
 const defaultTodos = [
   {text:'Levantarse', completed: true},
   {text:'Desayunar', completed: true},
-  {text:'Lavarse los dientes', completed: false}
+  {text:'Lavarse los dientes', completed: false},
+  {text:'Ir a la escuela', completed: true},
 ]
 function App() {
-  const [todos, setTodos] =React.useState([defaultTodos]);
+  const [todos, setTodos] =React.useState(defaultTodos);
   //Este estado debe estar en el componente padre, pues así debe ser la comunicación, de padres a hijos, no al revés.
   const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
 
   console.log('Los usuarios buscan todos de: ' +  searchValue)
 
@@ -21,7 +25,9 @@ function App() {
     //React.Fragment Permite tener un contenedor de todos nuestros compoenentes
     //<React.Fragment>
     <>
-      <TodoCounter completed={10} total={20}/>
+      <TodoCounter 
+      completed={completedTodos} 
+      total={totalTodos}/>
       <TodoSearch
        searchValue = {searchValue}
        setSearchValue = {setSearchValue}
