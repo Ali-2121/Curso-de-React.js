cbbb0427b2815e70da057dccd5c232f78b2d80c9
Ali-2121 <prds20bnll@gmail.com>
2025-01-12 17:02:35 -0600

    Clase 12: Función de eliminar y completar Todo's y cambiando el color de los íconos en hover

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -5,25 +5,20 @@ import { DeleteIcon } from '../icons/DeleteIcon';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <CompleteIcon completed={props.completed}/>
-      {/* <span 
-      className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-      onClick={props.onComplete}
-      >
-        V
-      </span> */}
+      <CompleteIcon 
+      completed={props.completed}
+      onComplete={props.onComplete}
+      />
+      
       {/* En este párrafo se agrega una clase solo si se recibe la propiedad completed como verdadero,
       en ese caso se agrega la clase para aplicar los estilos definidos solo cuando la actividad está completada */}
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <DeleteIcon/>
-      {/* <span 
-      className="Icon Icon-delete"
-      onClick={props.onDelete}
-      >
-        X
-      </span> */}
+      <DeleteIcon
+        onDelete={props.onDelete}
+      />
+      
     </li>
   );
 }
--- src/icons/TodoIcon.js
+++ src/icons/TodoIcon.js
@@ -6,10 +6,11 @@ const iconsTypes = {
     "check": (color) => <CheckSVG className='Icon-svg' fill={color}/>,
     "delete": (color) => <DeleteSVG className='Icon-svg' fill={color}/>
 }
-function TodoIcon({ type,color }){
+function TodoIcon({ type,color, onClick }){
     return(
         <span
         className={`Icon-container Icon-container-${type}`}
+        onClick={onClick}
         >
             {iconsTypes[type](color)}
         </span>
